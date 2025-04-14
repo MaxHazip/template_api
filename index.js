33853b9341ce54309efbd59003b29c48dd23f539
Студент группы IS <is@sielom.local>
2025-04-14 15:29:28 +0500

    databseConnection

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// const { creation } = "require"
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kkwuqmvyweilqfythrbh.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
