db68a144f4cf91fe66bc0a9609fa07fcf493fa85
Студент группы IS <is@sielom.local>
2025-04-16 11:11:39 +0500

    connection

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+const { createClient } = require('@supabase/supabase-js')
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://cloxomsbfwihntfvlepw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsb3hvbXNiZndpaG50ZnZsZXB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NzgwMDIsImV4cCI6MjA2MDM1NDAwMn0.pKdrP2ldYNJsU4zHPJYNRPW0zLc2rJQCrf0eTd-IdFc')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-// const { creation } = "require"
-import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kkwuqmvyweilqfythrbh.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+app.get('/students', async (req, res) => {
+  const { data, error } = await supabase
+  .from('students')
+  .select()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json(data)
+})
+app.post('/students', (req, res) => {
+  res.send('Добавление студентов')
+})
+app.delete('/students', (req, res) => {
+  res.send('Удаление студентов')
+})
+app.put('/students', (req, res) => {
+  res.send('Изменение студентов')
 })
 
 app.listen(port, () => {
